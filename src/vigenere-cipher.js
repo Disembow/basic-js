2bef0a87dc23278742560269a3b954a716e30b93
Disembow <e_naletov@mail.ru>
2022-10-17 15:26:45 +0200

    feat: add vigenere cipher solution

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -19,73 +19,102 @@ const { NotImplementedError } = require('../extensions/index.js');
  * reverseMachine.decrypt('AEIHQX SX DLLU!', 'alphonse') => '!NWAD TA KCATTA'
  *
  */
- class VigenereCipheringMachine {
-  encrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+
+const getAlphabet = ( letterCase ) => {
+  let alphabet = [];
+  let firstLetter;
+  letterCase === 'upper' ? firstLetter = 65 : letterCase === 'lower' ? firstLetter = 97 : '';
+  for (let i = 0; i <= 25; i++) {
+    alphabet.push(String.fromCharCode(firstLetter + i));
   }
-  decrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  return alphabet;
+};
+
+const getScaledKey = (str, key) => {
+  let keyToStrLength = '';
+  let keyScale = Math.ceil(str.length / key.length);
+  for (let i = 0; i < keyScale; i++) {
+    keyToStrLength += key;
+  };
+  keyToStrLength = keyToStrLength.substring(0, str.length);
+  return keyToStrLength;
+};
+
+// console.log(getScaledKey('attack at dawn', 'alphonse'));
+
+class VigenereCipheringMachine {
+  constructor(type) {
+    this.type = type;
+    this.type === undefined || this.type === true ? this.type = "direct" : this.type = "reverse";
   }
+
+  encrypt(str, key) {
+    if (!str || !key) {
+      throw new Error('Incorrect arguments!')
+    };
+    let alphabet = getAlphabet('lower');
+    let alphabetLength = alphabet.length;
+    let keyGrowed = getScaledKey(str, key).toLowerCase();
+    let result = '';
+    str = str.toLowerCase();
+
+    let i = 0;
+    let j = 0;
+    while (i < str.length) {
+      let strIndex = alphabet.indexOf(str[i]);
+      let alfabetIndex = alphabet.indexOf(keyGrowed[j]);
+      if (strIndex === -1) {
+        result += str[i];
+        i++;
+      } else {
+        result += alphabet[(strIndex + alfabetIndex) % alphabetLength];
+        i++;
+        j++
+      };
+    };
+    result = result.toUpperCase();
+    return this.type === "reverse" ? result.split('').reverse().join('') : result;
+  };
+
+  decrypt(encrStr, key) {
+    if (!encrStr || !key) {
+      throw new Error('Incorrect arguments!')
+    };
+
+    let alphabet = getAlphabet('lower');
+    let alphabetLength = alphabet.length;
+    let keyGrowed = getScaledKey(encrStr, key).toLowerCase();
+    let result = '';
+    encrStr = encrStr.toLowerCase();
+    let i = 0;
+    let j = 0;
+    while (i < encrStr.length) {
+      let strIndex = alphabet.indexOf(encrStr[i]);
+      let alfabetIndex = alphabet.indexOf(keyGrowed[j]);
+      if (strIndex === -1) {
+        result += encrStr[i];
+        i++;
+      } else {
+        result += alphabet[Math.abs(alphabetLength + (strIndex - alfabetIndex)) % alphabetLength];
+        i++;
+        j++
+      };
+    };
+    result = result.toUpperCase();
+    return this.type === "reverse" ? result.split('').reverse().join('') : result;
+  };
 }
-// const getAlphabet = ( letterCase ) => {
-//   let alphabet = [];
-//   let firstLetter;
-//   letterCase === 'upper' ? firstLetter = 65 : letterCase === 'lower' ? firstLetter = 97 : '';
-//   for (let i = 0; i < 25; i++) {
-//     alphabet.push(String.fromCharCode(firstLetter + i));
-//   }
-//   return alphabet;
-// };
-
-// const growKeyLength = (str, key) => {
-//   let keyToStrLength = '';
-//   let keyScale = Math.ceil(str.length / key.length);
-//   for (let i = 0; i < keyScale; i++) {
-//     keyToStrLength += key;
-//   }
-//   keyToStrLength = keyToStrLength.substring(0, str.length);
-//   return keyToStrLength;
-// }
-
-// console.log(growKeyLength('attack at dawn', 'alphonse'));
-
-// class VigenereCipheringMachine {
-
-
-//   encrypt(str, key) {
-//     if (!str || !key) {
-//       throw new Error('Incorrect arguments!')
-//     };
-//     let upperAlphabet = getAlphabet('upper');
-//     let lowerAlphabet = getAlphabet('lower');
-//     let keyGrowed = growKeyLength(str, key);
-//     let result = '';
-
-//     for (let i = 0; i < str.length; i++) {
-
-//     }
-
-
-//     return result;
-//   };
-
-//   decrypt(encrStr, key) {
-//     if (!encrStr || !key) {
-//       throw new Error('Incorrect arguments!')
-//     };
-
-//   };
-// }
 
 // const directMachine = new VigenereCipheringMachine()
-// const reverseMachine = new VigenereCipheringMachine(false)
-// console.log(directMachine.encrypt('attack all!', 'aabaac'))
+const reverseMachine = new VigenereCipheringMachine(false)
+// console.log(directMachine.encrypt('attack at dawn!', 'alphonse'))
 // console.log(directMachine.decrypt('UWJJW XAGWLNFM VNNNDXHVWWL :)', 'js'))
+// console.log('LEARN FRONTEND DEVELOPMENT :)');
+// console.log(directMachine.decrypt('UWJJW XAGWLNFM VNNNDXHVWWL :)', 'js'))
+// console.log('LEARN FRONTEND DEVELOPMENT :)');
 // console.log(directMachine.repeatString('lmao', 6))
-// console.log(reverseMachine.encrypt('attack all!', 'aabaac'))
-// console.log(reverseMachine.decrypt('UWJJW XAGWLNFM VNNNDXHVWWL :)', 'js'))
+console.log(reverseMachine.encrypt('attack all!', 'aabaac'))
+console.log(reverseMachine.decrypt('UWJJW XAGWLNFM VNNNDXHVWWL :)', 'js'))
 // console.log(reverseMachine.repeatString('lmao', 6))
 
 module.exports = {
